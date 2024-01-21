8c929ea93e2dcef0179dfe1a7d0c8f18b0c13727
sezin akbulut <sezin.akbulut@gmail.com>
2024-01-21 15:27:09 +0100

    update

--- index.php
+++ index.php
@@ -1,8 +1,7 @@
 <?php
 
-
 // Require the correct variable type to be used (no auto-converting)
-declare (strict_types=1);
+declare (strict_types = 1);
 
 // Show errors so we get helpful information
 ini_set('display_errors', '1');
@@ -16,16 +15,6 @@ require_once 'classes/CardRepository.php';
 
 $databaseManager = new DatabaseManager($config['host'], $config['user'], $config['password'], $config['dbname']);
 $databaseManager->connect();
-try {
-    $dbobj = new DatabaseManager();
-    $pdo = $dbobj->connect();
-
-    // $pdo for database operations
-
-
-} catch (Exception $e) {
-    echo "Error: " . $e->getMessage();
-}
 
 // This example is about a Pokémon card collection
 // Update the naming if you'd like to work with another collection
@@ -34,21 +23,13 @@ $cards = $cardRepository->get();
 
 // Get the current action to execute
 // If nothing is specified, it will remain empty (home should be loaded)
-// $action = $_GET['action'] ?? null;
-$page = $_SERVER["REQUEST_URI"];
-$BASE_PATH = "localhost:8080/BeCode/The-Mountain/php-crud";
+$action = $_GET['action'] ?? null;
 
 // Load the relevant action
 // This system will help you to only execute the code you want, instead of all of it (or complex if statements)
-switch ($page) {
-    case $BASE_PATH:
-        overview($databaseManager);
-        break;
-    case $BASE_PATH . 'create':
-        create($databaseManager);
-        break;
-    case $BASE_PATH . 'edit':
-        echo "Editing ...";
+switch ($action) {
+    case 'create':
+        create();
         break;
     default:
         overview();
@@ -62,23 +43,7 @@ function overview()
     require 'overview.php';
 }
 
-function create($databaseManager)
+function create()
 {
-    if (isset($_POST['submit'])) {
-        $cardRepository = new CardRepository($databaseManager);
-        $cardRepository->create();
-    }
-    require 'createView.php';
     // TODO: provide the create logic
 }
-/*
-function create(CardRepository $cardRepository)
-{
-    if ($_SERVER["REQUEST_METHOD"] === "POST" && isset($_POST['submit'])) {
-        // TODO: Handle form submission, extract form data, and call the create method
-        // Example: $cardRepository->create($_POST['name']);
-    }
-    // Load your create view
-    require 'createView.php';
-}
-*/
--- overview.php
+++ overview.php
@@ -1,29 +1,3 @@
-
-<?php
-/*
-require_once "classes/databaseManager.php";
-
-ini_set('display_errors', '1');
-ini_set('display_startup_errors', '1');
-error_reporting(E_ALL);
-
-try {
-    $dbobj = new DatabaseManager();
-    var_dump($dbobj);
-    $pdo = $dbobj->connect();
-
-    // $pdo for database operations
-
-
-} catch (Exception $e) {
-    echo "Error creating DatabaseManager instance: "  . $e->getMessage();
-}
-*/
-?>
-
-
-
-
 <!doctype html>
 <html lang="en">
 <head>
@@ -32,76 +6,15 @@ try {
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Goodcard - track your collection of Pokémon cards</title>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-    <script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
-    <!-- js file-->
-    <script src="./js/script.js"></script>
 </head>
 <body>
 
-<h1 class="bg-dark text-center text-light py-2">Goodcard - track your collection of Pokémon cards</h1>
-
-<div class="container">
-
-
-    <!-- Modal -->
-    <?php
-    include "form.php";
-    ?>
-    <?php
-    include "profile.php";
-    ?>
-
-    <!--input search and button section-->
-<div class="row mb-3">
-    <div class="col-10">
-        <div class="input-group">
-            <div class="input-group-prepend">
-                <span class="input-group-text bg-dark">
-                    <i class="fa fa-search text-light" aria-hidden="true"></i>
-                </span>
-            </div>
-            <input type="text" class="form-control" placeholder="Search...">
-        </div>
-    </div>
-    <div class="col-2">
-        <button class="btn btn-dark" type="button" data-toggle="modal" data-target="#newmodal">
-            Add New
-        </button>
-    </div>
-</div>
-
-
-    <!--table-->
-    <?php
-    require_once "table.php";
-    ?>
-
-    <!--pagination-->
-    <nav aria-label="Page navigation example" id="pagination">
-        <ul class="pagination justify-content-center">
-            <li class="page-item disabled"><a class="page-link" href="#">Previous</a></li>
-            <li class="page-item active"><a class="page-link" href="#">1</a></li>
-            <li class="page-item"><a class="page-link" href="#">2</a></li>
-            <li class="page-item"><a class="page-link" href="#">3</a></li>
-            <li class="page-item"><a class="page-link" href="#">Next</a></li>
-
-        </ul>
-    </nav>
-
-</div>
-
-
+<h1>Goodcard - track your collection of Pokémon cards</h1>
 
 <ul>
-
-   <?php foreach ($cards as $card) : ?>
+    <?php foreach ($cards as $card) : ?>
         <li><?= $card['name'] ?></li>
     <?php endforeach; ?>
-
 </ul>
 
 </body>
